ae773510a19025bb56c02b7136c899796755300b
Student <student@rysca25.local>
2025-10-22 09:50:19 +0000

    Added client and server configs and udp compile command

---
+++ udp/compile.sh
@@ -0,0 +1,3 @@
+rawnetcc ./udp_client udp.c udp_client.c ../ipv4/ipv4.c ../ipv4/ipv4_config.c ../ipv4/ipv4_route_table.c ../arp/arp.c ../eth/eth.c ../utils/rng.c
+rawnetcc ./udp_server udp.c udp_server.c ../ipv4/ipv4.c ../ipv4/ipv4_config.c ../ipv4/ipv4_route_table.c ../arp/arp.c ../eth/eth.c ../utils/rng.c
+
