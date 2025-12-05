59a3f83964e8d32808291440765c8c7ce2cf01f2
alex <alex@alexcuan.com>
2025-12-05 22:58:58 +0100

    Changed compiled file location (temporary)

--- ripv2/compile_client.sh
+++ ripv2/compile_client.sh
@@ -1,4 +1,5 @@
-rawnetcc /tmp/ripv2_client ripv2_client.c \
+# TODO: Cambiar localizacion del binario
+rawnetcc ./ripv2_client ripv2_client.c \
     ../udp/udp.c \
     ../ipv4/ipv4.c ../ipv4/ipv4_config.c ../ipv4/ipv4_route_table.c \
     ../arp/arp.c \
