f7897cffa15356bdb7b67290cb0a75c88438d5c3
alex <alex@alexcuan.com>
2026-09-16 17:43:35 +0200

    utils: sembrar el generador una sola vez por proceso

--- utils/rng.c
+++ utils/rng.c
@@ -1,10 +1,20 @@
 #include <stdlib.h>
 #include <time.h>
+#include <unistd.h>
 #include "rng.h"
+
+static int rng_initialized = 0;
 
 void rng_init()
 {
-    srand(time(NULL));
+    /* Idempotente: udp_open y los main() llaman aqui, y volver a sembrar con
+       time(NULL) dentro del mismo segundo reinicia la secuencia. El PID separa
+       ademas a dos procesos arrancados a la vez. */
+    if (!rng_initialized)
+    {
+        srand((unsigned int)time(NULL) ^ (unsigned int)getpid());
+        rng_initialized = 1;
+    }
 }
 
 int rng_get_rand_in_range(int min, int max)
